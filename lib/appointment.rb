9ddde747eed22e4ba5f5cdd38d2dd9d25237db78
Rachel S. Lee <rslee0025@gmail.com>
2020-06-14 03:10:41 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,26 +1,13 @@
 class Appointment
-
-  attr_reader :patient, :doctor, :date
-
-  @@all = []
-
-  def initialize(patient ="", doctor = "", date = "")
-    @patient = patient
-    @doctor = doctor
-    @date = date
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def patients
-    self.patient
-  end
-
-  def doctors
-    self.doctor
-  end
-
+    attr_accessor :date, :doctor, :patient
+    @@all = []
+    def initialize(date, patient, doctor)
+        self.date = date
+        self.patient = patient 
+        self.doctor = doctor
+        @@all << self
+    end
+    def self.all
+          @@all
+    end
 end
