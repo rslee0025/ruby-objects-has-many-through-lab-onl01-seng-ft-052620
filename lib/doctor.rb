68eec6ba776020d9d47d60bbe24d070166f4cddd
Rachel S. Lee <rslee0025@gmail.com>
2020-06-14 03:09:26 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor
+
+  attr_reader :name, :appointment, :patient
+
+  @@all_doc = []
+
+  def initialize(name)
+    @name = name
+    @@all_doc << self
+  end
+
+  def self.all
+    @@all_doc
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def songs 
+    Song.all.select {|song| song.genre == self}
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def artists
+    Song.all.map do |song|
+      song.artist
+    end
+  end
+
+end 
